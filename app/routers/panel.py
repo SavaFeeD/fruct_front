fb4ec75d63538af26e7b29b6f719faf8528bbadd
SavaFeeD <shachnev.02@mail.ru>
2021-12-12 11:49:59 +0700

    update

--- app/routers/panel.py
+++ app/routers/panel.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
+from hashlib import sha256
+import requests
+import random
 
 from starlette import status
 from starlette.responses import JSONResponse
 
 from app.dependencies import templates
-
 
 router = APIRouter(
     prefix='/panel',
@@ -16,11 +18,33 @@ router = APIRouter(
 
 @router.get("/", response_class=HTMLResponse)
 async def panel(request: Request):
+    wallet = sha256('word asd sushka'.encode('utf-8')).hexdigest()
+    trs = [{col: ''} for i in range(20) for col in ['from', 'to', 'price']]
+
+    for row in trs:
+        row['from'] = wallet
+        row['to'] = sha256(f'asd asd {random.randint(1, 200)} ds ds'.encode('utf-8')).hexdigest()
+        row['price'] = random.uniform(10.5, 75.5)
     return templates.TemplateResponse("panel.html", {
         "request": request,
         "wallet": {
-            "hash": "dqwdadsdsad",
+            "hash": wallet,
             "balance": "0.03123"
-        }
+        },
+        "transactions": trs,
     })
 
+
+@router.get("/transactions", response_class=HTMLResponse)
+async def transactions(request: Request):
+    wallet = sha256('word asd sushka'.encode('utf-8')).hexdigest()
+    trs = [{col: ''} for i in range(20) for col in ['from', 'to', 'price']]
+
+    for row in trs:
+        row['from'] = wallet
+        row['to'] = sha256(f'asd asd {random.randint(1, 200)} ds ds'.encode('utf-8')).hexdigest()
+        row['price'] = random.uniform(10.5, 75.5)
+    return templates.TemplateResponse("transactions.html", {
+        "request": request,
+        "transactions": trs,
+    })
